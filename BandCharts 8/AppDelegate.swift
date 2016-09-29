ae9afd62ba83951ea5d88f0eb1978b8a5bbcb833
Jim Hubbard <Jim@jims-macbook-pro.local>
2016-09-29 10:18:32 -0700

    Initial Commit

---
+++ BandCharts 8/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  BandCharts 8
+//
+//  Created by Jim Hubbard on 9/29/16.
+//  Copyright © 2016 Jim Hubbard. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ BandCharts 8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  BandCharts 8
+//
+//  Created by Jim Hubbard on 9/29/16.
+//  Copyright © 2016 Jim Hubbard. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
